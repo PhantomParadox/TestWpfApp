a640a79c96f35b921e0637509248fd53d7130d4f
amin mohseni <amin.mohseni.342@hotmail.com>
2019-10-15 16:29:38 +0330

    Its Ready for Test

---
+++ WpfTestApp/LogService/LogManager.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+
+namespace LogService
+{
+    public static class LogManager
+    {
+        public static IEnumerable<TSource> FromHierarchy<TSource>(this TSource source, Func<TSource, TSource> nextItem, Func<TSource, bool> canContinue)
+        {
+            for (var current = source; canContinue(current); current = nextItem(current))
+            {
+                yield return current;
+            }
+        }
+
+        public static IEnumerable<TSource> FromHierarchy<TSource>(this TSource source, Func<TSource, TSource> nextItem)
+            where TSource : class
+        {
+            return FromHierarchy(source, nextItem, s => s != null);
+        }
+
+        public static string GetAllMessages(this Exception exception)
+        {
+            var messages = exception
+                .FromHierarchy(ex => ex.InnerException)
+                .Select(ex => ex.Message);
+
+            return string.Join(Environment.NewLine, messages);
+        }
+        public static void Write(this Exception ex)
+        {
+            var method = new StackTrace().GetFrame(1).GetMethod();
+            string CallerInformation = string.Format("I was called from '{0}' of class '{1}'", method.Name, method.DeclaringType);
+            string exc = ex.GetAllMessages();
+            string date = $"{DateTime.Now.ToLongDateString()} {DateTime.Now.ToLongTimeString()}";
+
+            string result = $"\n\t {date} \n\t{CallerInformation} \n\t {exc}";
+
+            Debug.WriteLine(result);
+
+        }
+        public static string Show(this Exception ex)
+        {
+            var method = new StackTrace().GetFrame(1).GetMethod();
+            string CallerInformation = string.Format("I was called from '{0}' of class '{1}'", method.Name, method.DeclaringType);
+            string exc = ex.GetAllMessages();
+            string date = $"{DateTime.Now.ToLongDateString()} {DateTime.Now.ToLongTimeString()}";
+
+            string result = $"\n\t {date} \n\t{CallerInformation} \n\t {exc}";
+
+            Debug.WriteLine(result);
+            return result;
+
+        }
+    }
+}
---
+++ WpfTestApp/TestBlankApp1/App.xaml.cs
@@ -0,0 +1,43 @@
+﻿using TestBlankApp1.Views;
+using Prism.Ioc;
+using Prism.Modularity;
+using System.Windows;
+using LogService;
+using Unity;
+using Prism.Unity;
+
+namespace TestBlankApp1
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            try
+            {
+                base.OnStartup(e);
+            }
+            catch (System.Exception ex)
+            {
+                /// Change Catching the error 
+                ErrorView error = new ErrorView(ex.Show());
+                error.ShowDialog();
+
+                throw;
+            }
+        }
+
+        protected override Window CreateShell()
+        {
+            return Container.Resolve<MainWindow>();
+        }
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+            var con = containerRegistry.GetContainer();
+            con.AddExtension(new Diagnostic());
+        }
+    }
+}
